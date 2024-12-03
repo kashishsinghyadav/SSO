304c8de88a23ac014c9dae19b0dfa60cc1ba60ef
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2024-12-04 00:17:16 +0530

    Update admin.py

--- SingleSignOn/admin.py
+++ SingleSignOn/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import SSOProfile, SSOProvider
+
+@admin.register(SSOProvider)
+class SSOProviderAdmin(admin.ModelAdmin):
+    list_display = ('name', 'client_id', 'redirect_uri')  
+    search_fields = ('name', 'client_id')  
+    ordering = ('name',)  
+    list_filter = ('scopes',) 
+
+@admin.register(SSOProfile)
+class SSOProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'sso_provider', 'external_id', 'created_at', 'updated_at')  
+    search_fields = ('user__username', 'sso_provider', 'external_id') 
+    ordering = ('user', 'sso_provider') 
+    list_filter = ('sso_provider',)  
+
