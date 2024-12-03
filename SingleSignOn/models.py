cbe29ada08a6882011bb864ba6bb304e0b0d2ed5
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2024-12-03 23:31:30 +0530

    Update models.py

--- SingleSignOn/models.py
+++ SingleSignOn/models.py
@@ -1,4 +1,14 @@
-from django.db import models
+class SSOProvider(models.Model):
+    name = models.CharField(max_length=100)  
+    client_id = models.CharField(max_length=255)
+    client_secret = models.CharField(max_length=255)
+    auth_endpoint = models.URLField()  
+    token_endpoint = models.URLField()  
+    user_info_endpoint = models.URLField(blank=True, null=True)  
+    redirect_uri = models.URLField()  
+    scopes = models.TextField(default="openid email profile")  
 
-# Create your models here. 
+    def __str__(self):
+        return self.name
+
 
